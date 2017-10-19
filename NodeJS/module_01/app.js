fa3b3712a24bbb43fe417ecf0ed41e49654229dc
Lamuchy <ichibunny@hotmail.com>
2017-10-19 23:04:11 +0100

    Create app.js

---
+++ NodeJS/module_01/app.js
@@ -0,0 +1,8 @@
+var greet = require('./greet.js');
+
+// ./ is this dictionary
+// require is recived what module exported on the module file
+// so greet function from module is assigned to var greet on app.js
+// you can also omit .js using just greet instead. still works
+
+greet();
