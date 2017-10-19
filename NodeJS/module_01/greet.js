135c2d64e93cef6dc32824dea011fe917c08995a
Lamuchy <ichibunny@hotmail.com>
2017-10-19 23:03:09 +0100

    Create greet.js

---
+++ NodeJS/module_01/greet.js
@@ -0,0 +1,8 @@
+// module called greet
+
+var greet = function (){
+    console.log ('Hello!');
+};
+
+module.exports = greet;
+// give greet function to get exported ony can be set from module file
