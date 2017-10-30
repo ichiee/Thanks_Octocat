1b1e7a94a94296f2e494f60d99eb116c4076a402
Lamuchy <ichibunny@hotmail.com>
2017-10-30 19:17:54 +0000

    Create app.js

---
+++ NodeJS/npm_pacagejson/app.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+console.log(moment().format("ddd, hA"));
+
+/*
+this is how you use package from npm
+use require function to get 'moment, 
+it will look for the file or directory from Node core first then look for place like node_module
+
+moment is a package for time and date, see documentations on npm
+*/
