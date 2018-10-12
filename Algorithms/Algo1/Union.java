e8422c097fb0b7b4646af02314eb7586bda073bb
Lamuchy <ichibunny@hotmail.com>
2018-10-12 08:55:42 +0100

    Update Union.java

--- Algorithms/Algo1/Union.java
+++ Algorithms/Algo1/Union.java
@@ -13,17 +13,27 @@ Slow inplimention
 It's keeps state as on number of components {} {} {}
 
 you will use array with index as the actual value of the node.
-for value, it will be a secound ( latter ) param on the fanctions
+for value, it will be a secound ( latter ) param on the functions
 
 
+explanation of below
+Quickunion has constructor public QuickUnionUF
+It constsuct array that index and values are the same - this array is called parent
+
+helpers
+find method - this is == parent[p]- donno why they have to go through
+validate method - validate the int you add in is within the lengh of the index of the parent[]
+
+
+public method
+connected - return true if the value's are the same - the number they take is the number of index on parent []
 
 ////////////////////
 
 
-
-public class QuickUnionUF {
-    private int[] parent;  // parent[i] = parent of i
-    private int count;     // number of components
+public class QuickFindUF {
+    private int[] id;    // id[i] = component identifier of i
+    private int count;   // number of components
 
     /**
      * Initializes an empty union–find data structure with {@code n} sites
@@ -33,12 +43,11 @@ public class QuickUnionUF {
      * @param  n the number of sites
      * @throws IllegalArgumentException if {@code n < 0}
      */
-    public QuickUnionUF(int n) {
-        parent = new int[n];
+    public QuickFindUF(int n) {
         count = n;
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
+        id = new int[n];
+        for (int i = 0; i < n; i++)
+            id[i] = i;
     }
 
     /**
@@ -53,20 +62,18 @@ public class QuickUnionUF {
     /**
      * Returns the component identifier for the component containing site {@code p}.
      *
-     * @param  p the integer representing one object
+     * @param  p the integer representing one site
      * @return the component identifier for the component containing site {@code p}
      * @throws IllegalArgumentException unless {@code 0 <= p < n}
      */
     public int find(int p) {
         validate(p);
-        while (p != parent[p])
-            p = parent[p];
-        return p;
+        return id[p];
     }
 
     // validate that p is a valid index
     private void validate(int p) {
-        int n = parent.length;
+        int n = id.length;
         if (p < 0 || p >= n) {
             throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
         }
@@ -83,9 +90,10 @@ public class QuickUnionUF {
      *         both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
     public boolean connected(int p, int q) {
-        return find(p) == find(q);
+        validate(p);
+        validate(q);
+        return id[p] == id[q];
     }
-
   
     /**
      * Merges the component containing site {@code p} with the 
@@ -97,16 +105,22 @@ public class QuickUnionUF {
      *         both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
     public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootP == rootQ) return;
-        parent[rootP] = rootQ; 
+        validate(p);
+        validate(q);
+        int pID = id[p];   // needed for correctness
+        int qID = id[q];   // to reduce the number of array accesses
+
+        // p and q are already in the same component
+        if (pID == qID) return;
+
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pID) id[i] = qID;
         count--;
     }
 
     /**
      * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input, 
-     * where each integer represents some object;
+     * where each integer represents some site;
      * if the sites are in different components, merge the two components
      * and print the pair to standard output.
      *
@@ -114,7 +128,7 @@ public class QuickUnionUF {
      */
     public static void main(String[] args) {
         int n = StdIn.readInt();
-        QuickUnionUF uf = new QuickUnionUF(n);
+        QuickFindUF uf = new QuickFindUF(n);
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
@@ -124,7 +138,6 @@ public class QuickUnionUF {
         }
         StdOut.println(uf.count() + " components");
     }
-
 
 }
 
@@ -138,10 +151,10 @@ Quick Union
 
 Each entry array contains - referenceto the parent tree.
 
-From this data structure you can go to roote - 
+From this data structure you can go to root - 
 
-Once you know the rot you can impliment.
-So when it connect to one place to the another. All you need to is changing one value.
+Once you know the root you can impliment.
+So when it connects to one place to the another. All you need to is changing one value.
 
 example of implimention:
 Latte one will be added at the bototm of it
@@ -149,10 +162,10 @@ union (4, 3)
 union (3, 8)
 union (6, 5)
 
-So class QuickUnionUF have an anstructtor 
+So class QuickUnionUF have an constructtor 
 
 then finding root function
-check if the 2 entry p & q has the same root by using another method methofd to return boolean
+check if the 2 entry p & q has the same root by using another method  to return boolean
 
 Tree can become too tall
 
@@ -160,7 +173,7 @@ Tree can become too tall
 
 
 public class QuickUnionUF {
-    private int[] parent;  // parent[i] = parent of i
+    private int[] parent;  // parent[i] = parent of i // this is the same as id 
     private int count;     // number of components
 
     /**
@@ -171,7 +184,7 @@ public class QuickUnionUF {
      * @param  n the number of sites
      * @throws IllegalArgumentException if {@code n < 0}
      */
-    public QuickUnionUF(int n) {
+    public QuickUnionUF(int n) {.           // this is constructing parent array 
         parent = new int[n];
         count = n;
         for (int i = 0; i < n; i++) {
@@ -204,7 +217,7 @@ public class QuickUnionUF {
 
     // validate that p is a valid index
     private void validate(int p) {
-        int n = parent.length;
+        int n = parent.length;     // this would be array of the parent
         if (p < 0 || p >= n) {
             throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
         }
@@ -279,9 +292,127 @@ Path compression
 ///////////////////////
 
 
+public class WeightedQuickUnionUF {
+    private int[] parent;   // parent[i] = parent of i
+    private int[] size;     // size[i] = number of sites in subtree rooted at i
+    private int count;      // number of components
+
+    /**
+     * Initializes an empty union–find data structure with {@code n} sites
+     * {@code 0} through {@code n-1}. Each site is initially in its own 
+     * component.
+     *
+     * @param  n the number of sites
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public WeightedQuickUnionUF(int n) {
+        count = n;
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between {@code 1} and {@code n})
+     */
+    public int count() {
+        return count;
+    }
+  
+    /**
+     * Returns the component identifier for the component containing site {@code p}.
+     *
+     * @param  p the integer representing one object
+     * @return the component identifier for the component containing site {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     */
+    public int find(int p) {.   // this is find root the same as quick union
+        validate(p);
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    // validate that p is a valid index.  
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));  
+        }
+    }
+
+    /**
+     * Returns true if the the two sites are in the same component.   // if the roots are the same
+     *
+     * @param  p the integer representing one site
+     * @param  q the integer representing the other site
+     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Merges the component containing site {@code p} with the 
+     * the component containing site {@code q}.
+     *
+     * @param  p the integer representing one site
+     * @param  q the integer representing the other site
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public void union(int p, int q) {
+        int rootP = find(p);                // this is by finding root of p - that should the index == value of parent []
+        int rootQ = find(q);                
+        if (rootP == rootQ) return;
+
+        // make smaller root point to larger one
+        if (size[rootP] < size[rootQ]) {            // if index q of size has begger value
+            parent[rootP] = rootQ;                  // indes p of parent will take value / index of q
+            size[rootQ] += size[rootP];             // bigger size - size [index q] will have a value of size [p]
+        }
+        else {
+            parent[rootQ] = rootP;                  // otherwise do oppositeway around. either way a bigger size will be added more
+            size[rootP] += size[rootQ];
+        }
+        count--;
+    }
+
+
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input, 
+     * where each integer represents some object;
+     * if the sites are in different components, merge the two components
+     * and print the pair to standard output.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+}
 
 
 
+//////
 
 
 
